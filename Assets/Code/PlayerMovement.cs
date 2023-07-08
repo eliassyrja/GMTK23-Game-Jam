f0758ef33360d9ffbf1bc6669f88a439b5700950
Elias <elias.syrja@gmail.com>
2023-07-08 10:46:58 +0300

    Added simple player movement

--- Assets/Code/PlayerMovement.cs
+++ Assets/Code/PlayerMovement.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [SerializeField] private float movementSpeed;
+    [SerializeField] private Rigidbody2D rb;
+
+    Vector2 movement;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+    }
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movementSpeed * Time.fixedDeltaTime * movement);
     }
 }
