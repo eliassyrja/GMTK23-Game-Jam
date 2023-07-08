e75ad45c4b39f414621944ba8ccd9ae4db411bbd
Elias <elias.syrja@gmail.com>
2023-07-08 12:10:58 +0300

    Add health loss to PlayerHealth
    Loss speed increases gradually over time. Can and should be tweaked.
    Currently prints HP value to screen. Every 10 seconds loss speed gets faster.

--- Assets/Code/PlayerHealth.cs
+++ Assets/Code/PlayerHealth.cs
@@ -9,16 +9,29 @@ public class PlayerHealth : MonoBehaviour
 
     [SerializeField] private GameObject healthGameObject;
     [SerializeField] private float health;
+    [SerializeField] private float healthLossSpeed;
 
     // Start is called before the first frame update
     void Start()
     {
         healthText = healthGameObject.GetComponent<TextMeshProUGUI>();
+        InvokeRepeating("IncreaseHealthLossSpeed", 10, 10);
     }
 
     // Update is called once per frame
     void Update()
     {
-        healthText.text = health.ToString();
+        healthText.text = health.ToString("F0");
+        health -= healthLossSpeed;
+    }
+
+    void IncreaseHealthLossSpeed()
+    {
+        healthLossSpeed += 0.01f;
+    }
+
+    public float GetHealth()
+    {
+        return health;
     }
 }
