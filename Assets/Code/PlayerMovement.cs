2e9169588ea9668a18a4bdce189da5fbbdcb6d61
ieevari <125186933+ieevari@users.noreply.github.com>
2023-07-08 23:23:50 +0300

    Added turning

--- Assets/Code/PlayerMovement.cs
+++ Assets/Code/PlayerMovement.cs
@@ -6,6 +6,7 @@ public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float movementSpeed;
     [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private float rotationSpeed = 5f;
 
     private GameManager gm;
     private Vector2 movement;
@@ -21,6 +22,40 @@ public class PlayerMovement : MonoBehaviour
     {
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
+        if(movement.x < 0)
+		{
+            if (movement.y < 0)
+            {
+                transform.eulerAngles = Vector3.forward * 135;
+            }
+            else if (movement.y > 0)
+			{
+                transform.eulerAngles = Vector3.forward * 45;
+            }
+            else
+                transform.eulerAngles = Vector3.forward * 90;
+        }
+        else if (movement.x > 0)
+        {
+            if (movement.y < 0)
+            {
+                transform.eulerAngles = Vector3.forward * -135;
+            }
+            else if (movement.y > 0)
+            {
+                transform.eulerAngles = Vector3.forward * -45;
+            }
+            else
+                transform.eulerAngles = Vector3.forward * -90;
+        }
+        else if (movement.y < 0)
+        {
+            transform.eulerAngles = Vector3.forward * 180;
+        }
+        else if (movement.y > 0)
+        {
+            transform.eulerAngles = Vector3.forward;
+        }
     }
 
     void FixedUpdate()
@@ -29,6 +64,18 @@ public class PlayerMovement : MonoBehaviour
         {
             movement.Normalize();
             rb.MovePosition(rb.position + movementSpeed * Time.fixedDeltaTime * movement);
+            RotateInDirectionOfInput();
         }
     }
+
+    private void RotateInDirectionOfInput()
+	{
+        if (movement.x == 0 && movement.y == 0)
+		{
+            Quaternion targetRotation = Quaternion.LookRotation(transform.forward);
+            Quaternion rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+
+            rb.MoveRotation(rotation);
+		}
+	}
 }
