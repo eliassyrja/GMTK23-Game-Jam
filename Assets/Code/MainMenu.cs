212bb0b6164e09ea896ece10cec775bb22f3ebde
Elias <elias.syrja@gmail.com>
2023-07-09 13:13:03 +0300

    Removed unnecessary code from MainMenu script

--- Assets/Code/MainMenu.cs
+++ Assets/Code/MainMenu.cs
@@ -6,18 +6,9 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    private TextMeshProUGUI scoreText;
-    [SerializeField] private GameObject scoreGameObject;
-
     void Start()
     {
         gameObject.SetActive(true);
-    }
-
-    public void Setup(float score)
-    {
-        gameObject.SetActive(true);
-        scoreText.text = score.ToString("F0");
     }
 
     public void StartGame()
